0730ef0a800013f5c8e2a59d5f1aff8cd780b491
Thiago Menegotto <thiagolmenegotto@yahoo.com.br>
2022-03-14 11:12:43 -0300

    Update nextgocard.script.js

--- nextgocard.script.js
+++ nextgocard.script.js
@@ -1,34 +1,35 @@
+window.onload = function () {
+    document.querySelector('.extra_large').style.backgroundSize='contain';
+    var buttonlarge = document.querySelectorAll('.large');
 
-document.querySelector('.extra_large').style.backgroundSize='contain';
-var buttonlarge = document.querySelectorAll('.large');
-    
-for(i=0;i<buttonlarge.length;i++) {
-    buttonlarge[i].style.backgroundSize='contain';
-}
-            
-var product = document.querySelector('.product');
-product.style.padding="0";
-
-var buttonproduct = product.querySelector('a');
-var photo = document.querySelector('.photo');
-
-buttonproduct.style.width='auto';
-buttonproduct.style.backgroundSize='contain';
-buttonproduct.style.padding="0";
-buttonproduct.style.marginTop="0";
-buttonproduct.style.maxWidth="73%";
-
-
-function newQuery(query) {
-    if( query.matches ) {
-        photo.style.height="190px";
+    for(i=0;i<buttonlarge.length;i++) {
+        buttonlarge[i].style.backgroundSize='contain';
     }
-    else {
-        photo.style.height="294px";
+
+    var product = document.querySelector('.product');
+    product.style.padding="0";
+
+    var buttonproduct = product.querySelector('a');
+    var photo = document.querySelector('.photo');
+
+    buttonproduct.style.width='auto';
+    buttonproduct.style.backgroundSize='contain';
+    buttonproduct.style.padding="0";
+    buttonproduct.style.marginTop="0";
+    buttonproduct.style.maxWidth="73%";
+
+
+    function newQuery(query) {
+        if( query.matches ) {
+            photo.style.height="190px";
+        }
+        else {
+            photo.style.height="294px";
+        }
     }
+
+    var query = window.matchMedia( " (max-width: 375px) " )
+
+    newQuery(query)
+    query.addListener(newQuery)
 }
-
-var query = window.matchMedia( " (max-width: 375px) " )
-
-newQuery(query)
-query.addListener(newQuery)
