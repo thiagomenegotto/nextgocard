6d47081c6f10340d02b59e89fa8954c486f00eb3
Thiago Menegotto <thiagolmenegotto@yahoo.com.br>
2022-08-18 19:47:26 -0300

    Update nextgocard.smallbio-03.script.js

--- nextgocard.smallbio-03.script.js
+++ nextgocard.smallbio-03.script.js
@@ -3,8 +3,8 @@ window.onload = function () {
 // ***** FONTS *****
     
     var newStyle = document.createElement('style');
-    newStyle.appendChild(document.createTextNode('@font-face { font-family: '+Gilroy Font+'; src: url('+https://thiagomenegotto.github.io/nextgocard/tree/main/uploads/2022/07/Gilroy-Bold.woff2+');}'));
-
+    newStyle.appendChild(document.createTextNode('@font-face{font-family: '+Gilroy Font+'; src: url('+https://thiagomenegotto.github.io/nextgocard/tree/main/uploads/2022/07/Gilroy-Bold.woff2+');}'));
+    
     document.head.appendChild(newStyle);
     
     
