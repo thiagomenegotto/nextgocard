4ce157c3c00cc7642e2b9351d2c9da6a2fd36f37
Thiago Menegotto <thiagolmenegotto@yahoo.com.br>
2022-08-19 22:45:39 -0300

    Update nextgocard.smallbio-03.script.js

--- nextgocard.smallbio-03.script.js
+++ nextgocard.smallbio-03.script.js
@@ -102,9 +102,9 @@ window.onload = function () {
                 var desc_p = desc.querySelectorAll('p');
                 
                 for(j=0;j<desc_p.length;j++) {
-                    desc_p.style.fontFamily="Gilroy Font";
-                    desc_p.style.fontWeight="400";
-                    desc_p.style.textAlign="left";
+                    desc_p[j].style.fontFamily="Gilroy Font";
+                    desc_p[j].style.fontWeight="400";
+                    desc_p[j].style.textAlign="left";
                 }
             }
         }
