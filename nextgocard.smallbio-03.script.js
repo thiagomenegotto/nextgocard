438819b2cd1b58505b0fd0c88f78db276cbcebc6
Thiago Menegotto <thiagolmenegotto@yahoo.com.br>
2022-09-07 20:46:48 -0300

    Update nextgocard.smallbio-03.script.js

--- nextgocard.smallbio-03.script.js
+++ nextgocard.smallbio-03.script.js
@@ -290,10 +290,10 @@ window.onload = function () {
                 var container_wrapper = document.createElement('div');
                 
                 element_column.setAttribute("class","element-column");
-                element_column.setAttribute("style","z-index: 0; border-radius: 15px; border-color: linear-gradient(to top, #0ABAD1 padding-box, #5C4EE5 border-box); margin: 5px; backdrop-filter: blur(8px); width: 100%; display: flex; position: relative;");
+                element_column.setAttribute("style","border-radius: 15px; border: double 1px transparent; background-image: linear-gradient(white, white), linear-gradient(to top, #0ABAD1, #5C4EE5); background-origin: border-box; background-clip: content-box, border-box; margin: 5px; backdrop-filter: blur(8px); width: 100%; display: flex; position: relative;");
                                 
                 container_wrapper.setAttribute("class","container-wrapper");
-                container_wrapper.setAttribute("style","background-color: #E6E6E60D; padding: 50px 20px; border-radius: 15px; border-style: solid; border-color: linear-gradient(to top, #0ABAD1, #5C4EE5); border-width: 1px; transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s; margin: 0; display: flex; position: relative; width: 100%; flex-wrap: wrap; align-content: flex-start;");
+                container_wrapper.setAttribute("style","background-color: #E6E6E60D; padding: 50px 20px; transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s; margin: 0; display: flex; position: relative; width: 100%; flex-wrap: wrap; align-content: flex-start;");
                                              
                 container_wrapper.append(element_icon(0));
                 container_wrapper.append(element_icon(1));
